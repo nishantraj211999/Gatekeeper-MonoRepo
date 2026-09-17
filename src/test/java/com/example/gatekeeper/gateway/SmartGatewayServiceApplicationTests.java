8d3a4a5fcfd516f3dbebc6103b0e84d197094dba
Nishant2052 <nishantraj211999@gmail.com>
2026-09-18 02:11:40 +0530

    initial commit:Smart-Gateway Service code

---
+++ src/test/java/com/example/gatekeeper/gateway/SmartGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatekeeper.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
