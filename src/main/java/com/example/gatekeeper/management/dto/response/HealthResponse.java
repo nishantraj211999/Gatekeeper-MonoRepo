ee2f039551eaa3d5820fad843ff3fa8c6b53cfa7
Nishant2052 <nishantraj211999@gmail.com>
2026-09-18 02:08:00 +0530

    Initial commit: API Management Service code

---
+++ src/main/java/com/example/gatekeeper/management/dto/response/HealthResponse.java
@@ -0,0 +1,16 @@
+package com.example.gatekeeper.management.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class HealthResponse {
+    private String serviceName;
+    private String serviceDescription;
+    private String serviceStatus;
+}
